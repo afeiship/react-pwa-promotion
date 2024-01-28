91d4e4c3db4dfb0f6d5de45a619e691a0fab41ec
afeiship <1290657123@qq.com>
2024-01-28 23:15:03 +0800

    fix: need reload

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -62,6 +62,7 @@ export default class ReactPwaPromotion extends React.Component<ReactPwaPromotion
   updateServiceWorker = () => {
     this.swInstance.applyUpdate().then(() => {
       this.setNeedRefresh(false);
+      window.location.reload();
     });
   };
 
